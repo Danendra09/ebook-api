5b291f7f401efe9d900bc6077373ac15c1807670
XI RPL 2_03_Danendra A <84388509+Danendra09@users.noreply.github.com>
2022-11-08 13:25:29 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\HTTP\Controller\HeloController;
+use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +18,40 @@ use App\Http\Controllers\BookController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{id}', [BookController::class, 'show']);
-Route::post('/books', [BookController::class, 'store']);
-Route::put('/books/{id}', [BookController::class, 'update']);
-Route::delete('/books/{id}', [BookController::class, 'destroy']);
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('hello', function(){
+    return ["me" => "Ganteng"];
+});
+
+//Route::get('haloController', [HeloController::class,index]);
+//Route::post('haloController', [HeloController::class,store]);
+//Route::get('haloController', [HeloController::class,show]);
+//Route::put('haloController', [HeloController::class,update]);
+//Route::detele('haloController', [HeloController::class,destroy]);
+
+Route::resource('HalController', HeloController :: class);
+
+//Route::resource('siswa', SiswaController::class);
+//Route::resource('books', BookController::class);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//Public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::resource('siswa', SiswaController::class);
+Route::resource('books', BookController::class)->except('store', 'update');
+
+
+//protected routes
+Route::middleware('auth:sanctum')->group(function(){
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
